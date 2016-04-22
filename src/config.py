77a7cc7c53fe30297006dc6bf7daea56a2d56cfe
Curi Fabio <fcuri@arsat.com.ar>
2016-04-22 15:50:42 -0300

    first complete commit

---
+++ src/config.py
@@ -0,0 +1,73 @@
+
+__author__ = 'curif'
+
+config = {
+  #add your m3u8 providers here
+  "providers": {
+    #assign a name to each provider, example: "fromArchive", "fromURL", "fromMyProvider"
+    "fromArchive": {
+      "active": True,
+
+      #from file:
+      "url": "file:///home/desarrollo/desarr/cumulus-tv-m3u8-loader/src/test.m3u",
+
+      #map the group-title extinf data to android tv genres,
+      # see: http://developer.android.com/reference/android/media/tv/TvContract.Programs.Genres.html
+      "genres-map": {
+        "news": "NEWS",
+        "music": "MUSIC",
+        "movies": "MOVIES",
+        "entertainment": "ENTERTAINMENT",
+        "adult": "MOVIES",
+        "family": "FAMILY_KIDS",
+        "animal": "ANIMAL_WILDLIFE"
+      },
+    },
+    "MyGithubFileTest": {
+      "active": True,
+
+      #http url or file name in the form: "file:///home/<user>/blah/mifile.m3u"
+      "url": "https://raw.githubusercontent.com/curif/cumulus-tv-m3u8-loader/develop/src/my.m3u",
+
+      #some m3u url providers add some extra data at end url:
+      "m3u-url-endchar": "|",
+
+      #filter to include m3u data (all lowercase), None for ignore.
+      "filters": {
+        "lang": ["english", "spanish"],
+        "country": ["us", "ar", "au"],
+        #group-title
+        "group": [
+          "entertainment",
+          "news",
+          "family",
+          "animal"
+        ]
+      },
+      #map the group-title extinf data to android tv genres,
+      # see: http://developer.android.com/reference/android/media/tv/TvContract.Programs.Genres.html
+      "genres-map": {
+        "news": "NEWS",
+        "entertainment": "ENTERTAINMENT",
+        "family": "FAMILY_KIDS",
+        "animal": "ANIMAL_WILDLIFE"
+      }
+    }
+  },
+
+  "outputs": {
+    "google-drive": {
+      "active": True,
+      "file-name": "cumulus-tv-test.json",
+      "auth-code": ""
+    },
+    "m3u-file": {
+      "active": True,
+      "file-name": "cumulus-tv-test.m3u"
+    },
+    "json-file": {
+      "active": True,
+      "file-name": "cumulus-tv-test.json"
+    }
+  }
+}
---
+++ src/m3u8_loader.py
@@ -0,0 +1,261 @@
+
+import sys
+import urllib2
+import re
+import time
+import pprint
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import json
+
+try:
+  import config as config
+except ImportError:
+  print "config.py file not found! to create one copy the config_example.py over config.py and modify."
+  sys.exit(-1)
+
+"""
+Parser for Cumulus TV
+https://github.com/Fleker/CumulusTV
+"""
+
+"""
+m3u8 examples:
+
+#EXTINF:0 tvg-name="Important Channel" tvg-language="English" tvg-country="US" tvg-id="imp-001" tvg-logo="http://pathlogo/logo.jpg" group-title="Top10", Discovery Channel cCloudTV.ORG (Top10) (US) (English)
+http://167.114.102.27/live/Eem9fNZQ8r_FTl9CXevikA/1461268502/a490ae75a3ec2acf16c9f592e889eb4c.m3u8|User-Agent=Mozilla%2F5.0%20(Windows%20NT%206.1%3B%20WOW64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F47.0.2526.106%20Safari%2F537.36
+
+"""
+
+pp = pprint.PrettyPrinter(indent=2)
+
+__author__ = 'curif'
+
+m3u_regex = '#(.+?),(.+)\s*(.+)\s*'
+name_regex = '.*?tvg-name=[\'"](.*?)[\'"]'
+group_regex = '.*?group-title=[\'"](.*?)[\'"]'
+logo_regex = '.*?tvg-logo=[\'"](.*?)[\'"]'
+lang_regex = '.*?tvg-language=[\'"](.*?)[\'"]'
+country_regex = '.*?tvg-country=[\'"](.*?)[\'"]'
+id_regex = '.*?tvg-id=[\'"](.*?)[\'"]'
+
+m3uRe = re.compile(m3u_regex)
+nameRe = re.compile(name_regex)
+logoRe = re.compile(logo_regex)
+langRe = re.compile(lang_regex)
+countryRe = re.compile(country_regex)
+idRe = re.compile(id_regex)
+groupRe = re.compile(group_regex)
+
+
+def loadm3u(url):
+  hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+         'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+         'Accept-Encoding': 'none',
+         'Accept-Language': 'en-US,en;q=0.8',
+         'Connection': 'keep-alive'}
+
+  req = urllib2.Request(url, headers=hdr)
+  response = urllib2.urlopen(req)
+  data = response.read()
+
+  if not '#EXTM3U' in data:
+    raise Exception(url + " is not a m3u8 file.")
+
+  return data.encode('utf-8')
+
+
+def regParse(parser, data):
+  foundString = parser.search(data)
+  if foundString:
+    #print "regParse result", x.group(1)
+    return foundString.group(1).strip()
+  return None
+
+
+def mapGenres(genre, provider):
+  ret = ""
+  if genre:
+    genre = genre.lower()
+    genres = config.config["providers"][provider].get("genres-map", None)
+    if genres:
+      if genre in genres:
+        ret = genres[genre]
+  return ret
+
+
+def possibleGenres(cumulustv):
+  """
+  all genres in channels
+  :param cumulustv:
+  :return: genres array
+  """
+  retGenres = []
+  for channel in cumulustv["channels"]:
+    if "genres" in channel:
+      genres = channel["genres"].split(",")
+      for genre in genres:
+        if genre not in retGenres:
+          retGenres.append((genre))
+  return retGenres
+
+
+def process(m3u, provider, cumulustv, contStart=None):
+
+  match = m3uRe.findall(m3u)
+  urlEndChar = config.config["providers"][provider].get("m3u-url-endchar", "")
+  filters = config.config["providers"][provider].get("filters", None)
+
+  if contStart is None:
+    contStart=0
+
+  for extInfData, name, url in match:
+
+    id = regParse(idRe, extInfData)
+    logo = regParse(logoRe, extInfData)
+    country = regParse(countryRe, extInfData)
+    group = regParse(groupRe, extInfData)
+    lang = regParse(langRe, extInfData)
+
+    if name is None or name == "":
+      name = regParse(nameRe, extInfData)
+
+    if filters is None or \
+        (filters.get("country", None) is not None and country.lower() in filters["country"] and \
+         filters.get("lang", None) is not None and lang.lower() in filters["lang"] and \
+         filters.get("group", None) is not None and group.lower() in filters["group"]):
+
+      contStart += 1
+
+      if urlEndChar and urlEndChar != "":
+        url = url.split(urlEndChar)[0]
+
+      genres = mapGenres(group, provider)
+
+      cumulusData = {
+        "number": str(contStart),
+        "name": name,
+        "logo": logo,
+        "url": url,
+        "genres": genres,
+        "lang": lang, #extra data not defined in cumulus tv
+        "country": country #extra data not defined in cumulus tv
+      }
+      cumulustv["channels"].append(cumulusData)
+
+  return contStart
+
+
+def write2File(fd, cumulustv):
+  channels = cumulustv["channels"]
+  channelDataMap = [
+    ("number", "tvg-id"),
+    ("name", "tvg-name"),
+    ("logo", "tvg-logo"),
+    ("genres", "group-title"),
+    ("country", "tvg-country"),
+    ("lang", "tvg-language")
+  ]
+  fd.write("#EXTM3U\n")
+  for channel in channels:
+    fd.write("#EXTINF:0 ")
+    for dataId, extinfId in channelDataMap:
+      if channel[dataId] is not None and channel[dataId] != "":
+        fd.write(" " + extinfId + "=\"" + channel[dataId].strip() + "\" ")
+    fd.write(", " + channel["name"].strip() + "\n")
+    fd.write(channel["url"] + "\n")
+
+  return
+
+#-------------------------------------------------------------------------------
+# process
+#-------------------------------------------------------------------------------
+
+cumulustv = {"channels": [],
+             "timestamp": time.time()}
+startAt = 0  #first channel number - 1
+
+for provider, providerData in config.config["providers"].iteritems():
+  if providerData.get("active", False):
+    print "Provider: " + provider
+    try:
+      m3uContent = loadm3u(providerData["url"])
+    except Exception as e:
+      print str(e)
+    else:
+      startAt = int(providerData.get("first-channel-number", startAt))
+      startAt += process(m3uContent, provider, cumulustv, startAt)
+
+      genres = possibleGenres(cumulustv)
+      if len(genres) > 0:
+        cumulustv.update({"possibleGenres": genres})
+
+      #pp.pprint(cumulustv)
+
+print "Channels: " + str(startAt)
+
+#write to file
+m3uFile = config.config["outputs"].get("m3u-file", None)
+if m3uFile:
+  if m3uFile.get("active", False):
+    try:
+      fileName = m3uFile["file-name"]
+      with open(fileName, "w") as fd:
+        write2File(fd, cumulustv)
+      print "Output file: " + fileName
+    except Exception as e:
+      print "can't open/write file: " + str(e)
+      sys.exit(-1)
+
+#send to DRIVE
+driveConfig = config.config["outputs"].get("google-drive", None)
+if driveConfig:
+  if driveConfig.get("active", False):
+    gauth = GoogleAuth()
+    #gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication
+    authCode = driveConfig.get("auth-code", "")
+    if authCode is None or authCode == "":
+      url = gauth.GetAuthUrl()
+      print "This application have not access to your Google Drive. Yo need an access code from:"
+      print url
+      print "then copy and paste the code in \"auth-code\" in the outputs/google-drive section in your config.py file"
+      sys.exit(0)
+
+    try:
+      gauth.Auth(authCode)
+      drive = GoogleDrive(gauth)
+    except Exception as e:
+      print "Exception: " + str(e)
+      print "If you have problems with the application permissions try to use this url:"
+      print gauth.GetAuthUrl()
+      print "then copy and paste the code in the outputs/google-drive/auth-code section in your config.py file"
+      sys.exit(-1)
+
+    fileName = driveConfig.get("file-name", "cumulustv.json")
+    jsonContent = json.dumps(cumulustv, ensure_ascii=False)
+
+    try:
+      cumulusTVFile = drive.CreateFile({'title': fileName, 'mimeType': 'application/json'})  # Create GoogleDriveFile instance with title 'Hello.txt'
+      cumulusTVFile.SetContentString(jsonContent) # Set content of the file from given string
+      cumulusTVFile.Upload()
+      print "Uploaded to drive: " + fileName
+    except Exception as e:
+      print "Google Drive upload exception: " + str(e)
+
+#send json to disk
+jsonOutput = config.config["outputs"].get("json-file", None)
+if jsonOutput:
+  if jsonOutput.get("active", False):
+    fileName = jsonOutput.get("file-name", "cumulustv.json")
+    jsonContent = json.dumps(cumulustv, ensure_ascii=False)
+    try:
+      with open(fileName, "w") as fd:
+        fd.write(jsonContent)
+      print fileName + " saved."
+    except Exception as e:
+      print "ERROR saving json file: " + str(e)
+      sys.exit(-1)
+
+
+
